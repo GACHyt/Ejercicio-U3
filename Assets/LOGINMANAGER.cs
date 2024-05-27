f6f3be12da60a5df2244078e44a589f2e81ae537
GACHyt <146768965+GACHyt@users.noreply.github.com>
2024-05-27 16:00:12 -0300

    Password Check

--- Assets/LOGINMANAGER.cs
+++ Assets/LOGINMANAGER.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+//Cuando el usuario presione el boton "btn_Aceptar" debe escribirse un mensaje en la consola
+//Si el texto del inputPassword coincide con una contraseña predefiida por ustedes,
+//Escribir "Acces Granted", sino escribir "Acces Denied"
 
 public class LOGINMANAGER : MonoBehaviour
 {
+    public string Contraseña;
+    public Text InputContraseña;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,17 @@ public class LOGINMANAGER : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void MostrarMensaje()
+    {
+        if (InputContraseña.text==Contraseña)
+        {
+            Debug.Log("Acces Granted");
+        }
+        else
+        {
+            Debug.Log("Acces Denied");
+        }
     }
 }
